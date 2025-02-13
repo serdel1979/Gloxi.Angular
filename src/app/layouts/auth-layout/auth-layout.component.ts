dd512128e33ab83ec1b2c1fd15e84e182ae364d0
sergio <sdlbsso@gmail.com>
2025-02-13 12:32:18 -0300

    detalles-logo

--- src/app/layouts/auth-layout/auth-layout.component.ts
+++ src/app/layouts/auth-layout/auth-layout.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-auth-layout',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './auth-layout.component.html',
   styleUrl: './auth-layout.component.css'
 })
