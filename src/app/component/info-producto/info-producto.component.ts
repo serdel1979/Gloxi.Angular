e6ad64bf9269f0c26bc252c8c8a44ef66f70f868
Sergio De Luca <sdlbsso@gmail.com>
2025-03-23 22:01:07 -0300

    creacion infoproducto + grabacion efecto corregido

--- src/app/component/info-producto/info-producto.component.ts
+++ src/app/component/info-producto/info-producto.component.ts
@@ -17,18 +17,27 @@ export class InfoProductoComponent {
   videoBlobUrl: string | null = null;
 
   startRecording() {
+    this.videoElement.nativeElement.style.opacity = "0"; // Ocultar temporalmente el video
+  
     navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then(stream => {
       this.videoElement.nativeElement.srcObject = stream;
+      this.videoElement.nativeElement.muted = true;
+  
       this.mediaRecorder = new MediaRecorder(stream);
       this.mediaRecorder.ondataavailable = (event: { data: any; }) => this.recordedChunks.push(event.data);
       this.mediaRecorder.onstop = () => this.saveRecording();
       this.mediaRecorder.start();
+  
+      setTimeout(() => {
+        this.videoElement.nativeElement.style.opacity = "1"; // Mostrar el video después de un pequeño delay
+      }, 500);
     })
-    .catch(err=>{
+    .catch(err => {
       alert(err);
-    })
-    ;
+    });
   }
+  
+  
 
   stopRecording() {
     if (this.mediaRecorder) {
@@ -39,17 +48,30 @@ export class InfoProductoComponent {
         stream.getTracks().forEach(track => track.stop());
       }
   
-      this.videoElement.nativeElement.srcObject = null;
+      this.videoElement.nativeElement.srcObject = null; // Eliminar la vista previa en vivo
     }
   }
   
+  
+ 
 
   saveRecording() {
     const blob = new Blob(this.recordedChunks, { type: 'video/webm' });
     this.videoBlobUrl = URL.createObjectURL(blob);
+  
+    // Asignar el video grabado al elemento de video
+    this.videoElement.nativeElement.src = this.videoBlobUrl;
+    this.videoElement.nativeElement.controls = true;
+    this.videoElement.nativeElement.play();
+  
+    // Configurar el botón de descarga
     this.downloadLink.nativeElement.href = this.videoBlobUrl;
     this.downloadLink.nativeElement.download = 'video.webm';
+  
+    // Limpiar chunks grabados para futuras grabaciones
+    this.recordedChunks = [];
   }
+  
 
   handleFileInput(event: any) {
     const file = event.target.files[0];
--- src/app/pages/infoproducts/addinfoproduct/addinfoproduct.component.ts
+++ src/app/pages/infoproducts/addinfoproduct/addinfoproduct.component.ts
@@ -1,13 +1,58 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
 import { InfoProductoComponent } from "../../../component/info-producto/info-producto.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-addinfoproduct',
   standalone: true,
-  imports: [InfoProductoComponent],
+  imports: [InfoProductoComponent, CommonModule],
   templateUrl: './addinfoproduct.component.html',
   styleUrl: './addinfoproduct.component.css'
 })
 export class AddinfoproductComponent {
+
+
+  @ViewChild('tab1') tab1!: ElementRef;
+  @ViewChild('tab2') tab2!: ElementRef;
+  @ViewChild('tab3') tab3!: ElementRef;
+ 
+  tapActive = 1;
+  mostrarFormulario = false;
+
+  constructor(private renderer: Renderer2) {}
+
+
+
+  toNext() {
+    if(this.tapActive < 3){
+      this.tapActive = this.tapActive + 1;
+      this.selectTap(this.tapActive);
+    }else{
+      this.tapActive = 1;
+      this.selectTap(this.tapActive);
+    }
+  }
+
+
+  selectTap(tabIndex: number) {
+    if (tabIndex === 1) {
+      this.renderer.addClass(this.tab1.nativeElement, 'active');
+      this.renderer.removeClass(this.tab2.nativeElement, 'active');
+      this.renderer.removeClass(this.tab3.nativeElement, 'active');
+      this.tapActive = 1;
+    } else if (tabIndex === 2) {
+      this.renderer.addClass(this.tab2.nativeElement, 'active');
+      this.renderer.removeClass(this.tab1.nativeElement, 'active');
+      this.renderer.removeClass(this.tab3.nativeElement, 'active');
+      this.tapActive = 2;
+    }else if(tabIndex === 3){
+      this.renderer.addClass(this.tab3.nativeElement, 'active');
+      this.renderer.removeClass(this.tab2.nativeElement, 'active');
+      this.renderer.removeClass(this.tab1.nativeElement, 'active');
+      this.tapActive = 3;
+    }
+  }
+  
+  
 
 }
